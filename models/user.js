809efdd70c7f9597e9d6557f8edf1d84e39d1b97
Anmitchell <avarynmitchell@gmail.com>
2023-06-24 18:00:11 -0400

    Setting up user routes. Still needs work

--- models/user.js
+++ models/user.js
@@ -3,17 +3,19 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+// defines how data will be input into mongoDB
 const userSchema = new mongoose.Schema({
     id: {type: String, required: true},
     name: {type: String, required: true},
     dob: {type: String, required: true},
     address: {type: String, required: true},
     email: {type: String, required: true},
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     roles: {type: String, enum: ['teacher', 'student', 'admin'], required: true}
 })
 
+// encrypt password set by userSchema
 userSchema.pre('save', async function(next){
     this.isModified('password')?
     this.password = await bcrypt.hash(this.password, 8):
@@ -21,6 +23,7 @@ userSchema.pre('save', async function(next){
     next()
 })
 
+// generate token for user login recognition upon return login
 userSchema.methods.generateAuthToken = async function() {
     const token = jwt.sign({_id: this._id}, process.env.SECRET)
     return token
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,28 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/users')
 
-/****************/
-/*****INDUCES****/
-/****************/
-
-// Index
-router.get('/')
+// Index:
+router.get('/', userController.auth, userController.ShowAllUsers)
 
 // New - future update!
 
 // Delete
-router.delete('/:id')
+//router.delete('/:id', userController.auth, userController.deleteUser)
 
 // Update
-router.put('/')
+//router.put('/', userController.auth, userController.updateUser)
 
 // Create
-router.post('/')
+router.post('/', userController.createUser)
 
 // Edit - future update!
 
 // Show
-router.get('/:id')
+//router.get('/:id', userController.showUser)
+
+// Login
+router.post('login', userController.loginUser)
+
+// Logout
+router.post('logout', userController.auth, userController.logoutUser)
 
 module.exports = router
 
