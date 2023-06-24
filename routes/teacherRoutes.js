e501bf0f3584d30ec653d6ff550b445f78994e7b
Anmitchell <avarynmitchell@gmail.com>
2023-06-24 13:16:03 -0400

    server is set

---
+++ routes/teacherRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+/****************/
+/*****INDUCES****/
+/****************/
+
+// Index
+router.get('/')
+
+// New - future update!
+
+// Delete
+router.delete('/:id')
+
+// Update
+router.put('/')
+
+// Create
+router.post('/')
+
+// Edit - future update!
+
+// Show
+router.get('/:id')
+
+module.exports = router
+
