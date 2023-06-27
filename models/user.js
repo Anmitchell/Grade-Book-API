80aa6745f06b49bebe0e84d358beda434fd89ae5
Anmitchell <avarynmitchell@gmail.com>
2023-06-26 22:49:30 -0400

    Changed user model and added files for other models

--- models/user.js
+++ models/user.js
@@ -1,21 +1,23 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
+const { model, Schema } = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const secret = process.env.SECRET_KEY
 
-// defines how data will be input into mongoDB
-const userSchema = new mongoose.Schema({
-    id: {type: String, required: true},
+// Template for documents(objects) added to database
+const userSchema = new Schema({
     name: {type: String, required: true},
-    dob: {type: String, required: true},
-    address: {type: String, required: true},
-    email: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    roles: {type: String, enum: ['teacher', 'student', 'admin'], required: true}
+    role: {type: String, enum: ['admin', 'teacher', 'student']}
+}, {
+        timestamps: true
 })
 
-// encrypt password set by userSchema
+// Whenever documents(objects) are saved to the database
+// check to see if password has been changed
+// if password has been changed
+// re-encrypt password
 userSchema.pre('save', async function(next){
     this.isModified('password')?
     this.password = await bcrypt.hash(this.password, 8):
@@ -23,12 +25,12 @@ userSchema.pre('save', async function(next){
     next()
 })
 
-// generate token for user login recognition upon return login
+// generate token for user login recognition
 userSchema.methods.generateAuthToken = async function() {
-    const token = jwt.sign({_id: this._id}, process.env.SECRET)
+    const token = jwt.sign({_id: this._id}, secret)
     return token
 }
 
-const User = mongoose.model('User', userSchema)
+const User = model('User', userSchema)
 
 module.exports = User
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router()
 const userController = require('../controllers/users')
 
 // Index:
-router.get('/', userController.auth, userController.ShowAllUsers)
+router.get('/', userController.auth, userController.showAllUsers)
 
 // New - future update!
 
 // Delete
-//router.delete('/:id', userController.auth, userController.deleteUser)
+router.delete('/:id', userController.auth, userController.deleteUser)
 
 // Update
-//router.put('/', userController.auth, userController.updateUser)
+router.put('/:id', userController.auth, userController.updateUser)
 
 // Create
 router.post('/', userController.createUser)
@@ -19,13 +19,13 @@ router.post('/', userController.createUser)
 // Edit - future update!
 
 // Show
-//router.get('/:id', userController.showUser)
+router.get('/:id', userController.showUser)
 
 // Login
-router.post('login', userController.loginUser)
+router.post('/login', userController.loginUser)
 
 // Logout
-router.post('logout', userController.auth, userController.logoutUser)
+router.post('/logout', userController.auth, userController.logoutUser)
 
 module.exports = router
 
